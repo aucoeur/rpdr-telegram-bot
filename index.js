6aca3f2c732ed102aca14149e97edd987807f7bb
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-03-17 04:31:10 -0700

    Ready to run on heroku

---
+++ index.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+
+const token = process.env.TOKEN;
+
+const Bot = require('node-telegram-bot-api');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const packageInfo = require('./package.json');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+const server = app.listen(process.env.PORT, "0.0.0.0", () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Web server started at http://%s:%s', host, port);
+});
+
+let bot;
+
+if(process.env.NODE_ENV === 'production') {
+  bot = new Bot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
+}
+else {
+  bot = new Bot(token, { polling: true });
+}
+
+console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
+
+bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
+
+bot.on('message', (msg) => {
+  const name = msg.from.first_name;
+  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(() => {
+    // reply sent!
+  });
+});
+
+module.exports = (bot) => {
+  app.post('/' + bot.token, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+};
