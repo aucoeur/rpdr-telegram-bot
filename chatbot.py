6fedd4bd7bf187c5a216c82d8c3da6ba256952b9
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-03-18 22:38:33 -0700

    More training

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from chatterbot import ChatBot, comparisons
+from chatterbot import ChatBot, comparisons, filters
 from functools import wraps
 from utils import send_typing_action
 
@@ -12,11 +12,12 @@ chatbot = ChatBot('RPDR bot',
         # storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
         # database_uri='mongodb://localhost:27017/rpdr-chatterbot',
         logic_adapters=[{
-        'import_path': 'chatterbot.logic.BestMatch',
-        "statement_comparison_function": comparisons.levenshtein_distance,
-        'default_response': 'I think I misunderstood the assignment.',
-        'maximum_similarity_threshold': 0.5}],
-        tie_breaking_method="random_response"
+            'import_path': 'chatterbot.logic.BestMatch',
+            "statement_comparison_function": comparisons.levenshtein_distance,
+            'default_response': 'I think I misunderstood the assignment.',
+            'maximum_similarity_threshold': 0.5}],
+        tie_breaking_method="random_response",
+        filters=[filters.get_recent_repeated_responses]
     )
 
 @send_typing_action
--- train.py
+++ train.py
@@ -1,15 +1,12 @@
 from chatbot import chatbot
 from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 
-
-
 '''Only needs to be run once'''
-corpustrainer = ChatterBotCorpusTrainer(chatbot)
+# corpustrainer = ChatterBotCorpusTrainer(chatbot)
 
-corpustrainer.train(
-    "chatterbot.corpus.english.greetings",
-    "chatterbot.corpus.english.conversations"
-)
+# corpustrainer.train(
+#     "./data/rpdr.yml"
+# )
 
 trainer = ListTrainer(chatbot)
 
@@ -55,19 +52,22 @@ trainer.train([
     "Calm down Beyoncé"
 ])
 
-trainer.train([ "This was my moment!", "Good god girl get a grip"])   
+trainer.train(["Hey", "Hey Rusie Q"])
 
-trainer.train(["Sugar daddy", "Let's get it together before you wanna read. I don't have a sugar daddy, sweetheart. Everything that I've had, I've worked for, and I worked for to get and I've built myself. So you need to know that 100%. I don't have a sugar daddy, I've never had a sugar daddy. If I wanted a sugar daddy, yes, I probably can go out and get one, because I AM WHAT? SICKENING. You could never have a sugar daddy because you are NOT THAT KIND OF GIRL. Baby, everything I've had I worked for, and I've gotten myself. I built myself from the ground up, DUCKING BOTCH!"])
+trainer.train(["What are you doing?" , "Walking children in nature"])
 
-trainer.train(["I've never had a sugar daddy", 
-    "Let's get it together before you wanna read. I don't have a sugar daddy, sweetheart.", 
-   "Everything that I've had, I've worked for, and I worked for to get and I've built myself. So you need to know that 100%.", 
-   "I don't have a sugar daddy, I've never had a sugar daddy.", 
-   "If I wanted a sugar daddy, yes, I probably can go out and get one, because I AM WHAT? SICKENING.", 
-   "You could never have a sugar daddy because you are NOT THAT KIND OF GIRL.", "Baby, everything I've had I worked for, and I've gotten myself. I built myself from the ground up, DUCKING BITCH!"])
+trainer.train([ "This was my moment!", "Good god girl get a grip" ])   
+
+trainer.train([ "Sugar daddy", "Let's get it together before you wanna read. I don't have a sugar daddy, sweetheart. Everything that I've had, I've worked for, and I worked for to get and I've built myself. So you need to know that 100%. I don't have a sugar daddy, I've never had a sugar daddy. If I wanted a sugar daddy, yes, I probably can go out and get one, because I AM WHAT? SICKENING. You could never have a sugar daddy because you are NOT THAT KIND OF GIRL. Baby, everything I've had I worked for, and I've gotten myself. I built myself from the ground up, DUCKING BOTCH!"])
+
+trainer.train([ "I don't have a sugar daddy", 
+    "I've never had a sugar daddy.", 
+    "If I wanted a sugar daddy, yes, I probably can go out and get one",
+    "Because I AM WHAT? SICKENING.", 
+    "You could never have a sugar daddy because you are NOT THAT KIND OF GIRL",
+    "Baby, everything I've had I worked for, and I've gotten myself. I built myself from the ground up, DUCKING BITCH!"])
 
 trainer.train([ "Valentina",   
-    "You look like Linda Evangelista", 
     "You're perfect, you're beautiful, you look like Linda Evangelista! You're a model. Everything about you is perfect! Did you stone those tights? Oh, you're smiling! They eat her up EVERY SINGLE TIME she's on that damn stage. She could walk out there in a ducking diaper and they'll be like, 'Valentina! Your smile is beautiful!'",
     "I'm not gonna say nothing to that",  
     "Valentina your smile is beautiful", 
@@ -77,7 +77,7 @@ trainer.train([ "Valentina",
     "Did you stone those tights?", 
     "Oh you're smiling!", 
     "They eat her up every single time she's on that damned stage.", 
-    "She could walk out there in a ducking diaper and they'll be like, 'Valentina, Your smile is beautiful'", 
+    "She could walk out there in a ducking diaper and they'll be like", "Valentina, Your smile is beautiful", 
     "I'm not gonna say nothing to that" ])
 
 trainer.train([
@@ -102,7 +102,8 @@ trainer.train([
 
 trainer.train([
     "Who is this?", 
-    "I'm Jasmine Masters and I have something to say."
+    "I'm Jasmine Masters and I have something to say.",
+    "And I OOP"
 ]) 
 
 trainer.train([
@@ -117,19 +118,28 @@ trainer.train([
     
 trainer.train([
     "I'm Roxxxy Andrews and I'm here to make it clear."
-    "I know you love me baby, that's why you brought me here. Was a bitch on season 5 now I'm gonna make it right. Give me a sewing challenge and I'll give you what you like. I'm full of tricks, baby, just like on Halloween.  A room full of monsters and it makes me wanna scream! I have to get this right so you don't waste your time.  Not like my comedy, I'm killing on this rhyme. I'm gonna show you what I can do.  You're going crazy and seeing two.  It's not my fault, you can't blame my game.  All these other hoes but they're all the same.",
-    "Roxxxy Andrews",
+    "I know you love me baby, that's why you brought me here. Was a bitch on season 5 now I'm gonna make it right. Give me a sewing challenge and I'll give you what you like. I'm full of tricks, baby, just like on Halloween.  A room full of monsters and it makes me wanna scream! I have to get this right so you don't waste your time.  Not like my comedy, I'm killing on this rhyme. I'm gonna show you what I can do.  You're going crazy and seeing two.  It's not my fault, you can't blame my game.  All these other hoes but they're all the same."
+])
+
+trainer.train([
     "I'm Roxxxy Andrews and I'm here to make it clear.", 
-    "I know you love me baby, that's why you brought me here." , 
-    "Was a bitch on season 5 now I'm gonna make it right. ", 
-    "Give me a sewing challenge and I'll give you what you like." , 
-    "I'm full of tricks, baby, just like on Halloween. " , 
-    "A room full of monsters and it makes me wanna scream! ", 
-    "I have to get this right so you don't waste your time. " , 
-    "Not like my comedy, I'm killing on this rhyme. ", 
-    "I'm gonna show you what I can do.  ", 
-    "You're going crazy and seeing two."  , 
-    "It's not my fault, you can't blame my game. " , 
+    "I know you love me baby", 
+    "that's why you brought me here." , 
+    "Was a bitch on season 5", 
+    "now I'm gonna make it right.", 
+    "Give me a sewing challenge",
+    "and I'll give you what you like." , 
+    "I'm full of tricks, baby", 
+    "just like on Halloween." , 
+    "A room full of monsters",
+    "and it makes me wanna scream!", 
+    "I have to get this right", 
+    "so you don't waste your time.", 
+    "Not like my comedy", 
+    "I'm killing on this rhyme.", 
+    "I'm gonna show you what I can do. ", 
+    "You're going crazy and seeing two.", 
+    "It's not my fault, you can't blame my game.", 
     "All these other hoes but they're all the same."
 ])
 
@@ -142,3 +152,30 @@ trainer.train([
     "Sharon Needles", 
     "Go back to Party City where you belong!"
 ])
+
+trainer.train([
+    "You did", 
+    "But I kept going"
+])
+
+trainer.train([
+    "Are you okay?",
+    "Sometimes I don't understand anything about science"
+])
+
+trainer.train([ "Haha", "I'm not joking botch" ])
+
+trainer.train([ "Haha", "JOKES JOKES JOKES JOKES JOKES" ])
+
+trainer.train([
+    "Because I AM WHAT?",
+    "SICKENING.",
+    "Sickening, no?",
+    "BAM"])
+
+trainer.train([
+    "What do you do?", 
+    "Quickly"
+])
+
+trainer.train(["You're slow", "But I kept goin'!"])
